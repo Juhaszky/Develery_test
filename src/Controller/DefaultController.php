e2f4812228da7426921e123b34277b297be782ad
Juhász Krisztián István <juhasz.krisztian2000@gmail.com>
2021-05-20 13:40:59 +0200

    First Commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+class DefaultController {
+	public function homepage() {
+		return new Response('SZIASZTOK');
+	}
+}
